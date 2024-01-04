72637561e056c1735d86715942bd9e5042bfe2bb
Sławomir P <slawekpettke@gmail.com>
2024-01-04 11:41:06 +0100

    04/01/2024

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -1,8 +1,16 @@
 package Game;
 
+import Messages.Messages;
+
 public class Game {
+    private boolean endGame = false;
 
     public void start () {
+
+        System.out.println(Messages.INTRODUCTION);
+        System.out.println(Messages.INGREDIENTS);
+
+
 
     }
 }
--- src/main/java/Messages/Messages.java
+++ src/main/java/Messages/Messages.java
@@ -2,8 +2,23 @@ package Messages;
 
 public class Messages {
 
-    public final static String PRZEPIS = "Twoim zadaniem będzie przygotowanie jajecznicy. Wejdź do spiżarni i pobierz potrzebne składniki. \n" +
+    public static final String INTRODUCTION = "Twoim zadaniem będzie przygotowanie jajecznicy. Wejdź do spiżarni i pobierz potrzebne składniki. \n" +
             "Następnie przejdź do kuchni i używając odpowiednich narzędzi, przygotuj jajecznicę. ";
 
-    public final static String SKLADNIKI = "Składniki jajecznicy to: jajka, sól pieprz i masło.";
+    public static final String INGREDIENTS = "Składniki jajecznicy to: jajka, sól pieprz i masło.";
+    public static final String OPTIONS = "1 - użyj, 2 - zabierz, 3 - wyświetl przedmioty, 4 - pomoc, 5 - opis pomieszczenia, 0 - wyjdź";
+    public static final String KITCHEN_DESCRPT = "Jesteś w kuchni, odczuwasz ciepłą atmosferę, która unosi się w powietrzu. Światło słoneczne, wpadające przez okno, odbija się od błyszczących blatów, dodając wnętrzu energii i radości. Na lewo znajduje się kuchenka elektryczna.\n" +
+            "\n" +
+            "Zaraz obok niej stoi kolorowa miseczka gotowa do użycia. Obok miseczki leży patelnia.\n" +
+            "\n" +
+            "Na blacie stoi porcelanowy talerzyk, gotowy na przyjęcie najsmaczniejszych dań. Wokół niego rozsiane są kolorowe łyżki, widelce i noże, gotowe do użycia, przywołujące obraz radosnych posiłków i wspólnego czasu spędzonego przy stole.";
+
+    public static final String Pantry_DESCRPT = " Jesteś w spiżarni, znajdują się tu półki pełne jedzenia. W powietrzu unosi się mieszanka zapachów - świeżych ziół, przypraw i innych aromatycznych skarbów.\n" +
+            "\n" +
+            "Na półce w centralnej części spiżarni stoją kosze pełne świeżych jajek. Obok nich, w starannie oznakowanych pojemnikach, znajduje się sól morska i świeżo zmielony pieprz, gotowe, by dodać smaku każdemu przygotowanemu daniu.\n" +
+            "\n" +
+            "W jednym z kątów stoi słoik pełen domowego masła, które w ciepłe dni zachowuje swój delikatny zapach i konsystencję. Obok niego leży miseczka, w której mieszka niedawno starty ser, emanujący intensywnym, kuszącym aromatem.\n" +
+            "\n" +
+            "Półki są również ozdobione słoiczkami z suszonymi ziołami, które dodają spiżarni wyjątkowego uroku. Ich różnorodność kolorów i intensywnych zapachów dodaje magii każdej potrawie, jaką można stworzyć z zawartości tej przytulnej spiżarni. Całe pomieszczenie jest jak skarbnica smaków, gotowa do wykorzystania w kulinarnych eksperymentach.";
+
 }
